6d098519ed4a4df60ea5468fe5b28c63d19d674a
Mie <dotam1236@gmail.com>
2022-04-20 15:32:14 +0700

    bindingData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 
+type Element = {
+  type: string;
+  name: string;
+  content: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
-  styles: [
-    `
-      h1 {
-        color: dodgerblue;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Tam';
+  serverElements: Element[] = [
+    { type: 'server', name: 'testServer', content: 'just a test' },
+  ];
 }
