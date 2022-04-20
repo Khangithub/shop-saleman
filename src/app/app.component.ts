5ea0808b6abbe7f5d1c6559d595b7a8dcbfc2194
Mie <dotam1236@gmail.com>
2022-04-20 15:52:52 +0700

    allias and binding custom event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,28 @@ type Element = {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   serverElements: Element[] = [
     { type: 'server', name: 'testServer', content: 'just a test' },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBluePrintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
