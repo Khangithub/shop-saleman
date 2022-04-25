85f489f78b15a058abe5489ea62aca99d40a6883
Mie <dotam1236@gmail.com>
2022-04-25 14:23:34 +0700

    practice ngFor and passing data to childComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,14 @@
 import { Component } from '@angular/core';
 
-type Element = {
-  type: string;
-  name: string;
-  content: string;
-};
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements: Element[] = [
-    { type: 'server', name: 'testServer', content: 'just a test' },
-  ];
+  loadedFeature = 'recipe';
 
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
-    });
-  }
-
-  onBluePrintAdded(blueprintData: {
-    serverName: string;
-    serverContent: string;
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent,
-    });
-  }
-
-  onChangeFirst() {
-    this.serverElements[0].name = 'Changed!';
-  }
-
-  deleteFirstElement() {
-    this.serverElements.splice(0, 1);
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
   }
 }
