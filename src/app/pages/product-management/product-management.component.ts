730bc8acc463eb9c2286b08f29912b142497a958
Mie <dotam1236@gmail.com>
2022-05-13 18:24:36 +0700

    add product ui

--- src/app/pages/product-management/product-management.component.ts
+++ src/app/pages/product-management/product-management.component.ts
@@ -4,24 +4,30 @@ import { HttpClient } from "@angular/common/http";
 @Component({
   selector: "app-product-management",
   templateUrl: "./product-management.component.html",
-  styleUrls: ["./product-management.component.css"],
+  styleUrls: ["./product-management.component.scss"],
 })
 export class ProductManagementComponent implements OnInit {
-  selectedFiles = [];
+  private productImage: any[] = [];
+  private name: string = "";
+  private manufacturer: string = "";
+  private price: number = 0;
+  private discount: number = 0;
+  private category: string = "";
+  private description: string = "";
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   onFileChange(event) {
-    this.selectedFiles = event.target.files;
+    this.productImage = event.target.files;
   }
 
   onSubmit() {
-    console.log("submitting")
+
     const fd = new FormData();
-    for (let i = 0; i < this.selectedFiles.length; i++) {
-      fd.append("product-media", this.selectedFiles[i]);
+    for (let i = 0; i < this.productImage.length; i++) {
+      fd.append("product-media", this.productImage[i]);
     }
 
     this.http
